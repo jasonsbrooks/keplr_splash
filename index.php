7d1b654af9c56727f6a8cd0ed102320742b328c4
Kirill Miniaev <KM@Kirills-MacBook-Pro.local>
2013-05-21 11:48:26 -0400

    Finished html and css and jquery pages

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 
 	<!-- META -->
-	<title>TITLE</title>
+	<title>Keplr</title>
 	<link rel="shortcut icon" href="images/favicon.ico" />
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<meta name="description" content="TITLE" />
@@ -15,14 +15,49 @@
 	<!--SCRIPTS -->	
 	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
 	<script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
+
+	<script type="text/javascript" src="//use.typekit.net/vsx3pwj.js"></script>
+	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
 	<script src="scripts/code.js"></script>
+
+	<!--[if gte IE 9]>
+	  <style type="text/css">
+	    .gradient {
+	       filter: none;
+	    }
+	  </style>
+	<![endif]-->
 	
 </head>
 <body>
-	<form id="subscribe" action="<?=$_SERVER['PHP_SELF']; ?>" method="get">
-		<input id="email" type="email" name="email" value="your@emailaddress.com" />
-		<button type="submit" id="subscribe_submit" class="gradient">SUBMIT</button>
-		<span id="alert"><? require_once('inc/store-address.php'); if($_GET['submit']){ echo storeAddress(); } ?></span>
-	</form>
+	<div id="universe">
+		<HEADER>
+			<object id="keplr_logo" type="image/svg+xml" data="../images/keplr_logo.svg"></object>
+		</HEADER>
+		<SECTION id="galaxy">
+			<p><span class="nm-400">Keplr</span> is a new idea. A new <span class="nm-400">horizon</span>. We want to reimagine the world of scientific collaboration, and the frontiers of collaborator identification. We strive to bring <span class="nm-400">order</span> to the universe of research <span class="nm-400">information</span>.</p>
+			<p>Here comes our <span class="nm-400">supernova</span>.</p>
+			<p><span class="nm-395">3</span> . . . <span class="nm-405">2</span> . . . <span class="nm-410">1</span> . . .</p>
+		</SECTION>
+		<form id="subscribe" action="<?=$_SERVER['PHP_SELF']; ?>" method="get">
+			<input id="email" type="email" name="email" value="submit your email for news updates" class="nm-730" /><!--
+			--><button id="engageHyperJets" type="submit" title="Submit your email"></button>
+			<span id="alert"><? require_once('inc/store-address.php'); if($_GET['submit']){ echo storeAddress(); } ?>&mdash;Thank you, rockstar!&mdash;
+			</span>
+		</form>
+	</div>
+	<div id="aurora">
+		<div id="yellow" class="borealis gradient"></div><!--
+		--><div id="orange" class="borealis gradient"></div><!--
+		--><div id="red" class="borealis gradient"></div><!--
+		--><div id="pink" class="borealis gradient"></div><!--
+		--><div id="purple" class="borealis gradient"></div><!--
+		--><div id="blue" class="borealis gradient"></div><!--
+		--><div id="teal" class="borealis gradient"></div><!--
+		--><div id="green" class="borealis gradient"></div>
+	</div>
 </body>
 </html>
+
+
--- scripts/code.js
+++ scripts/code.js
@@ -1,42 +1,40 @@
+/**************************************************/
+/******** Kirill Miniaev | Miniaev Design *********/
+/** kirill@miniaevdesign.com | miniaevdesign.com **/
+/**************************************************/
+
 $(document).ready(function() {
 	
-	
 	//Animation on load
-	$('#about').hide().delay(500).fadeIn();
-	$('#subscribe').hide().delay(1000).fadeIn();
-	$('#description').hide().delay(1500).fadeIn();
-	
-	//Hiding Description sections from view
-	$('#description_content').hide();
-	$('#hider').hide();
+	// $('header').hide().delay(500).fadeIn();
+	// $('#galaxy').hide().delay(1200).fadeIn();
+	// $('#subscribe').hide().delay(2000).fadeIn();
+
+	// $('#yellow').hide().delay(2500).fadeIn();
+	// $('#orange').hide().delay(2700).fadeIn();
+	// $('#red').hide().delay(2900).fadeIn();
+	// $('#pink').hide().delay(3100).fadeIn();
+	// $('#purple').hide().delay(3300).fadeIn();
+	// $('#blue').hide().delay(3500).fadeIn();
+	// $('#teal').hide().delay(3700).fadeIn();
+	// $('#green').hide().delay(3900).fadeIn();
+
 	
 	//Hiding alert
 	$('#alert').hide();
 	
-	//View Selection Notes
-	$('#viewer').click(function() {
-		$('#description_content').show("slide", { direction: "up" }, 500);
-		$('#hider').show();
-		$(this).hide();
-	});
-	
-	//Hide Selection Notes
-	$('#hider').click(function() {
-		$('#description_content').hide("slide", { direction: "up" }, 500);
-		$(this).hide();
-		$('#viewer').delay(500).fadeIn();
-	});
-	
 	//Form Preperation
 	$('#email').focus(function() {
-		if ( $(this).val() == 'your@emailaddress.com' || $(this).val() == '') {
+		if ( $(this).val() == 'submit your email for news updates' || $(this).val() == '') {
 				$(this).val('');
+				$(this).switchClass('nm-730', 'nm-750', 0);
 		}
 	});
 	
 	$('#email').blur(function() {
 		if ( $(this).val() == '') {
-				$(this).val('your@emailaddress.com');
+				$(this).val('submit your email for news updates');
+				$(this).switchClass('nm-750', 'nm-730', 0);
 		}
 	});
 	
@@ -45,7 +43,7 @@ $(document).ready(function() {
 		// update user interface
 		var cur_val = $('#email').val();
 		
-		if ( (cur_val != "your@emailaddress.com") && (cur_val != "") ) {
+		if ( (cur_val != "submit your email for news updates") && (cur_val != "") ) {
 			$('#alert').fadeIn();
 			$('#alert').html('Adding email address...');
 			
@@ -55,7 +53,7 @@ $(document).ready(function() {
 				data: 'ajax=true&email=' + escape($('#email').val()),
 				success: function(msg) {
 					if (msg = 'Success! Check your email to confirm sign up.') {
-						$('#email').val('your@emailaddress.com');
+						$('#email').val('submit your email for news updates');
 					}
 					$('#alert').html(msg);
 					$('#alert').delay(2000).fadeOut();
